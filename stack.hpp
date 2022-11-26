ed98a02101c97b8c83528d7474387db349edce3e
Zakaria Wina <zwina@e2r10p7.1337.ma>
2022-11-26 15:19:02 +0100

    stack

---
+++ stack.hpp
@@ -0,0 +1,70 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+namespace ft
+{
+  template < class T, class Container = ft::vector<T> >
+    class stack
+    {
+      public:
+        typedef typename ft::vector<T>::value_type      value_type;
+        typedef typename ft::vector<T>::size_type       size_type;
+        typedef typename ft::vector<T>::reference       reference;
+        typedef typename ft::vector<T>::const_reference const_reference;
+        typedef ft::vector<T>                           container_type;
+
+      private:
+        container_type _c;
+
+      public:
+        explicit stack ( const container_type & ctnr = container_type() ) : _c(ctnr)    { }
+
+        stack( const stack& other ) : _c(other._c)                                      { }
+
+        stack& operator=( const stack & other )                                         { _c = other._c; return (*this); }
+
+        ~stack()                                                                        { }
+
+        bool empty() const                                                              { return ( _c.empty() ); }
+
+        size_type size() const                                                          { return ( _c.size() ); }
+
+        value_type&       top()                                                         { return ( _c.back() ); }
+        const value_type& top() const                                                   { return ( _c.back() ); }
+
+        void push ( const value_type& val )                                             { _c.push_back( val ); }
+
+        void pop()                                                                      { _c.pop_back(); }
+
+        void swap ( stack & x )                                                         { _c.swap(x._c); }
+
+        template < class U, class _Container >
+          friend bool operator== ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+        template < class U, class _Container >
+          friend bool operator!= ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+        template < class U, class _Container >
+          friend bool operator<  ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+        template < class U, class _Container >
+          friend bool operator<= ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+        template < class U, class _Container >
+          friend bool operator>  ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+        template < class U, class _Container >
+          friend bool operator>= ( const stack<U,_Container>& lhs, const stack<U,_Container>& rhs );
+    };
+  template < class T, class Container >
+    bool operator== ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c == rhs._c ); }
+  template < class T, class Container >
+    bool operator!= ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c != rhs._c ); }
+  template < class T, class Container >
+    bool operator<  ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c < rhs._c ); }
+  template < class T, class Container >
+    bool operator<= ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c <= rhs._c ); }
+  template < class T, class Container >
+    bool operator>  ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c > rhs._c ); }
+  template < class T, class Container >
+    bool operator>= ( const stack<T,Container>& lhs, const stack<T,Container>& rhs ) { return ( lhs._c >= rhs._c ); }
+}
+
+#endif
